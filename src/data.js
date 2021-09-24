6223daf642d7d89ddecd323871682d8e6c90c665
Hanifmuh <mulyawan.hanif@gmail.com>
2021-09-24 23:05:47 +0700

    tambahan

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Nasi Gorong',
+        price: 12000,
+        image: 'https://fodmap-publicsite-us-east-2.s3.amazonaws.com/production/media/images/Nasi_Goreng.original.jpg',
+      },
+      {
+        id: '2',
+        name: 'Mie Goreng',
+        price: 7000,
+        image: 'https://s4.bukalapak.com/img/4577845591/w-1000/INDOMIE_GORENG_SELERAKU_MIE_INSTANT__MMA_112_.jpg',
+      },
+      {
+        id: '3',
+        name: 'Kwetiaw',
+        price: 10000,
+        image: 'https://i.ytimg.com/vi/ZI6qVUk2pZQ/maxresdefault.jpg',
+      },
+      {
+        id: '4',
+        name: 'Nasi Ayam Bali',
+        price: 13000,
+        image: 'https://img.qraved.co/v2/image/data/2015/08/25/nook-640x640-x.jpg',
+      },
+      {
+        id: '5',
+        name: 'Es Teh',
+        price: 3000,
+        image: 'https://img.okezone.com/content/2020/09/27/481/2284512/5-penyakit-yang-mengintai-saat-anda-terlalu-banyak-minum-es-teh-YAytooa2HO.jpg',
+      },
+      {
+        id: '6',
+        name: 'Nutrisari',
+        price: 4000,
+        image: 'https://cf.shopee.com.my/file/cba6070a3958e76ee023a9f51a849348',
+      },
+      {
+        id: '7',
+        name: 'Kopi Panas',
+        price: 3000,
+        image: 'https://www.sukmulsismamedika.com/wp-content/uploads/2019/03/Benarkah-Kopi-Panas-Bisa-Picu-Kanker-Esofagus.jpg',
+      },
+      {
+        id: '8',
+        name: 'Aqua',
+        price: 3500,
+        image: 'https://brandplus-live-v1-content-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/media/uploads/20201106101032/Aqua_1500ml.jpg',
+      },
+    ],
+  };
+  export default data;
